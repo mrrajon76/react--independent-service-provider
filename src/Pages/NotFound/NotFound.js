d45841dd850f88c35d7ea3733160e04687c342a1
Md. Rasheduzzaman Rajon <mrrajon76@gmail.com>
2022-04-18 02:12:21 +0600

    NotFound page added

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import notFound from '../../images/404-error.gif';
 
 const NotFound = () => {
     return (
-        <div>
-
+        <div className='flex justify-center bg-slate-100'>
+            <Link to='/'><img src={notFound} alt="" className='' /></Link>
         </div>
     );
 };
