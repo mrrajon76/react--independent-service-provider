21adc1d9da040be171ffb5cf061b18c80b4fd602
Md. Rasheduzzaman Rajon <mrrajon76@gmail.com>
2022-04-18 21:07:24 +0600

    Contact form added

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -35,6 +35,31 @@ const Contact = () => {
                         referrerpolicy="no-referrer-when-downgrade" />
                 </div>
             </div>
+            <div className='mt-16'>
+                <h3 className='text-4xl lg:text-5xl text-indigo-900 font-extrabold text-center mb-6'>Have a Query?</h3>
+
+                <form>
+                    <div className="grid grid-cols-1 md:grid-cols-2 md:gap-16">
+                        <div className="relative z-0 mb-6 w-full group">
+                            <input type="email" name="floating_email" className="block py-2.5 px-0 w-full bg-transparent border-0 border-b-2 border-slate-400 appearance-none focus:outline-none focus:ring-0 focus:border-indigo-900 peer" placeholder=" " required />
+                            <label for="floating_email" className="absolute  text-gray-900 dark:text-gray-600 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-indigo-900 peer-focus:dark:text-indigo-900ss peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Your Name</label>
+                        </div>
+                        <div className="relative z-0 mb-6 w-full group">
+                            <input type="text" name="floating_name" className="block py-2.5 px-0 w-full bg-transparent border-0 border-b-2 border-slate-400 appearance-none focus:outline-none focus:ring-0 focus:border-indigo-900 peer" placeholder=" " required />
+                            <label for="floating_name" className="absolute  text-gray-900 dark:text-gray-600 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-indigo-900 peer-focus:dark:text-indigo-900ss peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email address</label>
+                        </div>
+                    </div>
+                    <div className="relative z-0 mb-6 w-full group">
+                        <input type="text" name="floating_subject" className="block py-2.5 px-0 w-full bg-transparent border-0 border-b-2 border-slate-400 appearance-none focus:outline-none focus:ring-0 focus:border-indigo-900 peer" placeholder=" " required />
+                        <label for="floating_subject" className="absolute  text-gray-900 dark:text-gray-600 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-indigo-900 peer-focus:dark:text-indigo-900ss peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Subject</label>
+                    </div>
+                    <div className="relative z-0 mb-6 w-full group">
+                        <textarea rows={4} name="floating_message" className="block py-2.5 px-0 w-full bg-transparent border-0 border-b-2 border-slate-400 appearance-none focus:outline-none focus:ring-0 focus:border-indigo-900 peer" placeholder=" " required />
+                        <label for="floating_message" className="absolute  text-gray-900 dark:text-gray-600 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-indigo-900 peer-focus:dark:text-indigo-900ss peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Your Message</label>
+                    </div>
+                    <button type="submit" className="text-white bg-indigo-900 hover:bg-amber-500 focus:outline-none uppercase rounded px-16 py-2 text-center">Send</button>
+                </form>
+            </div>
         </div>
     );
 };
